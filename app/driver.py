77376d62402557d0d20f661e46a4e2eb504dd49a
Gui <guicostaantunes@gmail.com>
2023-10-31 15:10:13 +0000

    Driver moved to new file

---
+++ app/driver.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+class Driver():
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.page = self.driver.get("https://www.mercadao.pt/store/pingo-doce")
+        self.driver.maximize_window()
+
+    def close(self):
+        self.driver.close()
+
+    def reject_cookies(self):
+        try:
+            button = WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located((By.XPATH, "//div[@id=\'onetrust-button-group\']/div[3]/button"))
+            )
+            button.click()
+            self.driver.find_element(By.XPATH, "//div[@id=\'onetrust-pc-sdk\']/div/div[3]/div/button").click()   
+        except TimeoutException:
+            pass
--- app/main.py
+++ app/main.py
@@ -1,42 +1,22 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-
+from driver import Driver
 import websockets
 import ssl
 import asyncio
+import json
 
 HOST = "localhost"
 
-class Driver():
+def process_message(message: str):
+    if message == "OK":
+        return "OK"
+    else:
+        json_message = json.loads(message)
+        print("Command: ", json_message["text"])
+        return json_message
 
-    def __init__(self):
-        self.driver = webdriver.Chrome()
-        self.page = self.driver.get("https://www.mercadao.pt/store/pingo-doce")
-        self.driver.maximize_window()
-
-    def close(self):
-        self.driver.close()
-
-    def reject_cookies(self):
-        try:
-            button = WebDriverWait(self.driver, 5).until(
-                EC.presence_of_element_located((By.XPATH, "//div[@id=\'onetrust-button-group\']/div[3]/button"))
-            )
-            button.click()
-            self.driver.find_element(By.XPATH, "//div[@id=\'onetrust-pc-sdk\']/div/div[3]/div/button").click()   
-        except TimeoutException:
-            pass
-
-async def im1_message_handler(message):
-    # Implemente a lógica do manipulador de mensagens aqui
+async def message_handler(driver: Driver, message: str):
+    message = process_message(message)
     print(f"Received message: {message}")
-
-async def socket_open_handler():
-    # Implemente a lógica do manipulador de abertura do socket aqui
-    print("WebSocket is open")
 
 async def main():
 
@@ -45,16 +25,21 @@ async def main():
 
     mmi_cli_out_add = f"wss://{HOST}:8005/IM/USER1/APP"
 
+    # SSL config
     ssl_context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
     ssl_context.check_hostname = False
     ssl_context.verify_mode = ssl.CERT_NONE
 
-    async with websockets.connect(mmi_cli_out_add, ssl=ssl_context) as websocket:
-        print("Connected to WebSocket")
+    try:
+        async with websockets.connect(mmi_cli_out_add, ssl=ssl_context) as websocket:
+            print("Connected to WebSocket")
 
-        while True:
-            message = await websocket.recv()
-            await im1_message_handler(message)
+            while True:
+                message = await websocket.recv()
+                await message_handler(driver, message)
+    
+    except Exception as e:
+        print(f"Error: {e}")
 
     driver.close()
 
