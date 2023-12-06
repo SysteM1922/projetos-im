9e2e2927bbb29c67cb078061e9c6de00b9af121d
Gui <guicostaantunes@gmail.com>
2023-12-06 15:16:43 +0000

    New Libraries

--- start.ps1
+++ start.ps1
@@ -1,16 +1,7 @@
-Set-Location -Path .\mmiframeworkV2
+Set-Location -Path .\IM
 Start-Process -FilePath .\start.bat
 
 Set-Location -Path ..\FusionEngine
 Start-Process -FilePath .\start.bat
 
 Set-Location -Path ..
-.\venv\Scripts\activate
-#conda activate rasa-env
-Set-Location -Path .\rasaDemo
-Start-Process -FilePath rasa -ArgumentList 'run', '--enable-api', '-m', '.\models\', '--cors', '*'
-
-Set-Location -Path ..\WebAppAssistantV2
-Start-Process -FilePath .\start_web_app.bat
-Set-Location -Path ..
-Start-Process "chrome.exe" "https://127.0.0.1:8082/index.htm"
